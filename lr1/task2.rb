aaf8fe2e27add0ae45883b7d9773d2c19f4ba209
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2021-03-09 22:09:06 +0300

    lr1 task 2-4

--- lr1/task2.rb
+++ lr1/task2.rb
@@ -1,3 +1,18 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hello " + name
+puts "Hello, #{ARGV[0]}"
+
+puts "Your favorite language, #{ARGV[0]}?"
+language = STDIN.gets.chomp
+
+if language == "ruby"
+    puts "you podliza"
+else
+    puts "Skoro budet ruby"
+end
+
+puts "#{ARGV[0]}, input command of OS?"
+oscommand = STDIN.gets.chomp
+system(oscommand)
+
+puts "#{ARGV[0]}, input command of RUBY?"
+rubycommand = STDIN.gets.chomp
+system("ruby -e \"#{rubycommand}\"")
