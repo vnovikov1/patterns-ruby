04da33ee8a2e2c2be3ff342f2ab374006217977b
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2021-03-12 22:30:27 +0300

    lr2 task3 way1

---
+++ lr2/task3.rb
@@ -0,0 +1,14 @@
+def input(amount)
+    amount = amount.to_i
+    arr = Array.new
+
+    puts "Введите список из #{amount} элементов: "
+    for item in 1..amount
+        item = STDIN.gets.chomp
+        arr.push(item)
+    end
+
+    return arr
+end
+
+print input(ARGV[0])
