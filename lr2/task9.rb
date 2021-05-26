88223d46c21270efd5b6228808b0b41f4d0c3181
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2021-05-26 16:10:27 +0300

    lr2 task9_1

---
+++ lr2/task9.rb
@@ -0,0 +1,17 @@
+def input 
+    print "Введите строку: "
+    str = gets.chomp
+    return str
+end
+
+def russianSym(str)
+    k = 0
+    bytes = []
+    str.each_byte {|symbol| bytes.append(symbol)}
+    for item in bytes
+        if item >= 128 && item <= 239
+            k += 1
+        end
+    end
+    return k
+end
