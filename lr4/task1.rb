d31a27a3e486d9776eb3dc4afc2cb9d76edda4c3
vnovikov1 <63616307+vnovikov1@users.noreply.github.com>
2021-06-26 01:11:26 +0300

    lr4 correct date

--- lr4/task1.rb
+++ lr4/task1.rb
@@ -22,6 +22,32 @@ class Phone
     def to_s
         return self.brand + " - " + self.model.to_s + " - " + self.release_date
     end
+
+    def check_release_date
+        if self.release_date =~ (/([0-2]\d|3[0-1]).(0[1-9]|1[0-2]).(\d{4})/)
+            return true
+        end
+        return false
+    end
+
+    def correct_date
+        if !check_release_date
+          new_val = self.release_date.split(".")
+          if new_val[0].to_i > 31 or new_val[1].to_i > 12 or new_val[2].size > 4
+            raise "Uncorrect date"
+            else
+                if new_val[0].to_i < 10 and new_val[0].size == 1
+                    new_val[0].insert(0, '0')
+                end
+
+                if new_val[1].to_i < 10 and new_val[1].size == 1
+                    new_val[1].insert(0, '0')
+                end
+            end
+          self.release_date= new_val.join(".")
+        end
+    end
+    
 end
 
 iphone = Phone.new("apple", "iphone 11", "1.12.2021", "black", 75000, "6 inch", "64 gb", "4 gb", "ios", "12 mp")
